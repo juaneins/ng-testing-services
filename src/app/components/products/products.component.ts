55fde8f4e76b352b5eeabe976a3e0b89808eda9f
juaneins_uio <ballesterosjg@gmail.com>
2023-04-22 17:03:26 -0500

    copy models, service from angular router project

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ProductsService } from '../../services/products.service';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.productsService.getAllSimple().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
