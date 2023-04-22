1479485dccc6af9a051db93f9f00a0962ad4f4dc
juaneins_uio <ballesterosjg@gmail.com>
2023-04-22 14:06:58 -0500

    create jazmine spy for mock

--- src/app/services/master.service.spec.ts
+++ src/app/services/master.service.spec.ts
@@ -15,4 +15,13 @@ describe('MasterService', () => {
     const masterService = new MasterService(fake as ValueService);
     expect(masterService.getValue()).toBe('fake from obj');
   });
+
+  it('should call getValue from the ValueService', () => {
+    const valueServiceSpy = jasmine.createSpyObj('ValueService', ['getValue']);
+    valueServiceSpy.getValue.and.returnValue('fake value');
+    const masterService = new MasterService(valueServiceSpy);
+    expect(masterService.getValue()).toBe('fake value');
+    expect(valueServiceSpy.getValue).toHaveBeenCalled();
+    expect(valueServiceSpy.getValue).toHaveBeenCalledTimes(1);
+  });
 });
