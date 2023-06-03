72b3cb5a09e96e2d7f96a130be1d3531a555346f
juaneins_uio <ballesterosjg@gmail.com>
2023-06-03 15:24:41 -0500

    test components with inputs

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PersonComponent } from './person.component';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Person } from '../../models/person.model';
 
 fdescribe('PersonComponent', () => {
   let component: PersonComponent;
@@ -21,25 +22,46 @@ fdescribe('PersonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should have <p> with text I am a paragraph', () => {
+
+  it('should have name "Juan"', () => {
+    component.person = new Person('Juan', 'Perez', 41, 80, 1.65);
+    expect(component.person.name).toEqual('Juan');
+  });
+
+  it('should have <p> with text my height is ${component.person.height}', () => {
     // use debugElement by agnostic
+    // arrange
+    component.person = new Person('Mario', 'Perez', 33, 80, 1.65);
+    const expectMsg = `my height is ${component.person.height}`;
     const personDebug: DebugElement = fixture.debugElement;
     const personElement: HTMLElement = personDebug.nativeElement;
     const p = personElement.querySelector('p');
-    expect(p?.textContent).toEqual('I am a paragraph');
+    // act
+    fixture.detectChanges();
+    // assert
+    expect(p?.textContent).toEqual(expectMsg);
   });
-  it('should have <p> with text I am a paragraph css selector', () => {
+  it('should have <p> with text my height is {{ person.height }} css selector', () => {
     // use debugElement by agnostic
+    component.person = new Person('Mario', 'Perez', 33, 80, 1.65);
+    const expectMsg = `my height is ${component.person.height}`;
     const personDebug: DebugElement = fixture.debugElement;
     const pDebug: DebugElement = personDebug.query(By.css('p'));
     const pElement: HTMLElement = pDebug.nativeElement;
-    expect(pElement?.textContent).toEqual('I am a paragraph');
+    fixture.detectChanges();
+    expect(pElement?.textContent).toEqual(expectMsg);
   });
-  it('should have <h3> with text Hello, PersonComponent', () => {
+  it('should have <h3> with text Hello, {{person.name}}', () => {
     // use debugElement by agnostic
+    // arrange
+    component.person = new Person('Mario', 'Perez', 33, 80, 1.65);
+    const expectMsg = `Hello, ${component.person.name}`;
     const personDebug: DebugElement = fixture.debugElement;
     const h3Debug: DebugElement = personDebug.query(By.css('h3'));
     const h3Element: HTMLElement = h3Debug.nativeElement;
-    expect(h3Element?.textContent).toEqual('Hello, PersonComponent');
+    // act
+    fixture.detectChanges();
+    // assert
+    expect(h3Element?.textContent).toEqual(expectMsg);
   });
 });
--- src/app/components/person/person.component.ts
+++ src/app/components/person/person.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Person } from 'src/app/models/person.model';
 
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.scss']
+  styleUrls: ['./person.component.scss'],
 })
 export class PersonComponent {
-
+  @Input() person: Person = new Person('', '', 0, 0, 0);
 }
