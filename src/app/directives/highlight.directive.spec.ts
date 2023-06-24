b1f68c17137e5018307efd2bb4d2d1a2c034b882
juaneins_uio <ballesterosjg@gmail.com>
2023-06-24 06:59:48 -0500

    test directives with ngModel

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -3,14 +3,21 @@ import { HighlightDirective } from './highlight.directive';
 import { Component, DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
 @Component({
-  template: ` <h3>Directive</h3>
+  template: `
+    <h3>Directive</h3>
     <h5 class="title" highlight>There is a value</h5>
     <h5 highlight="green">Green</h5>
     <p highlight="blue">Paragraph</p>
-    <p>Another Paragraph</p>`,
+    <p>Another Paragraph</p>
+    <input type="text" [(ngModel)]="color" [highlight]="color" />
+  `,
 })
-class HostComponent {}
+class HostComponent {
+  color = 'pink';
+}
 
 fdescribe('HighlightDirective', () => {
   let component: HostComponent;
@@ -19,6 +26,7 @@ fdescribe('HighlightDirective', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HostComponent, HighlightDirective],
+      imports: [FormsModule],
     }).compileComponents();
   });
 
@@ -39,8 +47,8 @@ fdescribe('HighlightDirective', () => {
     const elementsWithout = fixture.debugElement.queryAll(
       By.css('*:not([highlight])')
     );
-    expect(elements.length).toEqual(3);
-    expect(elementsWithout.length).toEqual(2);
+    expect(elements.length).toEqual(4);
+    expect(elementsWithout.length).toEqual(3);
   });
 
   it('should the elements march with bgColor', () => {
@@ -50,7 +58,7 @@ fdescribe('HighlightDirective', () => {
     expect(elements[0].nativeElement.style.background).toEqual('gray');
     expect(elements[1].nativeElement.style.background).toEqual('green');
     expect(elements[2].nativeElement.style.background).toEqual('blue');
-    expect(elements.length).toEqual(3);
+    expect(elements.length).toEqual(4);
   });
 
   it('should the h5.title be default bgColor', () => {
@@ -59,5 +67,20 @@ fdescribe('HighlightDirective', () => {
     expect(title.nativeElement.style.background).toEqual(
       directive.defaultColor
     );
+  });
+
+  it('should bind <input> and change the bgColor', () => {
+    const inputDebug = fixture.debugElement.query(By.css('input'));
+    const inputElement: HTMLInputElement = inputDebug.nativeElement;
+
+    expect(inputElement.style.backgroundColor).toEqual('pink');
+
+    inputElement.value = 'green';
+    inputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(inputElement.style.backgroundColor).toEqual('green');
+    // ng model tambien cambia el valor del componente
+    expect(component.color).toEqual('green');
   });
 });
