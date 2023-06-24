9bcbeb3d92e0258bf4a3e8911a8caf32a7fbc7cf
juaneins_uio <ballesterosjg@gmail.com>
2023-06-24 07:50:22 -0500

    Testing pipes in components

--- src/app/pipes/reverse.pipe.spec.ts
+++ src/app/pipes/reverse.pipe.spec.ts
@@ -1,4 +1,10 @@
 import { ReversePipe } from './reverse.pipe';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
 
 fdescribe('ReversePipe', () => {
   it('create an instance', () => {
@@ -16,5 +22,56 @@ fdescribe('ReversePipe', () => {
     const pipe = new ReversePipe();
     const rta = pipe.transform('123');
     expect(rta).toEqual('321');
+  });
+});
+
+@Component({
+  template: `
+    <h5>{{ 'amor' | reverse }}</h5>
+    <input type="text" [(ngModel)]="text" />
+    <p>{{ text | reverse }}</p>
+  `,
+})
+class HostComponent {
+  text = '';
+}
+
+fdescribe('ReversePipe from HostComponent', () => {
+  let component: HostComponent;
+  let fixture: ComponentFixture<HostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HostComponent, ReversePipe],
+      imports: [FormsModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should be h5 be roma', () => {
+    const h5Debug = fixture.debugElement.query(By.css('h5'));
+    expect(h5Debug.nativeElement.textContent).toEqual('roma');
+  });
+
+  it('should apply reverse pipe when typing in the input', () => {
+    const inputDebug = fixture.debugElement.query(By.css('input'));
+    const inputNativeElement: HTMLInputElement = inputDebug.nativeElement;
+    const pDebug = fixture.debugElement.query(By.css('p'));
+
+    expect(pDebug.nativeElement.textContent).toEqual('');
+    inputNativeElement.value = 'bear';
+    inputNativeElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(pDebug.nativeElement.textContent).toEqual('raeb');
   });
 });
